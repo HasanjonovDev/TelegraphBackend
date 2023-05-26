67a17f9e4eec4995dd61674272427be9e62da3cd
Azamat <hasanjonovazamat@gmail.com>
2023-05-26 15:15:35 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/telegraphbackend/TelegraphBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.telegraphbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegraphBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegraphBackendApplication.class, args);
+    }
+
+}
