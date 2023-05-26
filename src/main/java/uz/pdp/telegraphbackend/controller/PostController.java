132a23aec34c50ba0835b8280ba899b0a01f2e1c
Azamat <hasanjonovazamat@gmail.com>
2023-05-26 17:48:30 +0500

    Post Add

--- src/main/java/uz/pdp/telegraphbackend/controller/PostController.java
+++ src/main/java/uz/pdp/telegraphbackend/controller/PostController.java
@@ -1,12 +1,33 @@
 package uz.pdp.telegraphbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import uz.pdp.telegraphbackend.dto.PostCreateDto;
+import uz.pdp.telegraphbackend.entity.PostEntity;
+import uz.pdp.telegraphbackend.service.PostService;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/post")
 public class PostController {
+    private final PostService postService;
 
+    @PostMapping("/add")
+    public ResponseEntity<PostEntity> add(
+            @RequestBody PostCreateDto postCreateDto,
+            @RequestParam UUID ownerId
+            ){
+        PostEntity post = postService.add(postCreateDto, ownerId);
+        if (post!=null){
+             return ResponseEntity.ok(post);
+        }
+        return null;
+    }
 }
--- src/main/java/uz/pdp/telegraphbackend/service/PostService.java
+++ src/main/java/uz/pdp/telegraphbackend/service/PostService.java
@@ -1,7 +1,37 @@
 package uz.pdp.telegraphbackend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uz.pdp.telegraphbackend.dto.PostCreateDto;
+import uz.pdp.telegraphbackend.entity.PostEntity;
+import uz.pdp.telegraphbackend.entity.UserEntity;
+import uz.pdp.telegraphbackend.repository.PostRepository;
+import uz.pdp.telegraphbackend.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    public PostEntity add(PostCreateDto postCreateDto, UUID userId){
+        PostEntity postEntity = modelMapper.map(postCreateDto, PostEntity.class);
+        Optional<UserEntity> user = userRepository.findById(userId);
+        UserEntity userEntity = user.orElse(null);
+        if (userEntity!=null){
+            String name = userEntity.getName().replaceAll(" ","");
+            String link = name+"-"+ LocalDateTime.now();
+            postEntity.setOwner(userEntity);
+            postEntity.setLink(link);
+            return postRepository.save(postEntity);
+        }else {
+            return null;
+        }
+    }
 }
